25f0c32266468f9c575fc8fded294ad16b54b4e9
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-20 21:17:22 +0900

    test4 Part Write

--- SpringBoot_G04/src/main/java/com/ezen/spg04/FormController.java
+++ SpringBoot_G04/src/main/java/com/ezen/spg04/FormController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,14 +26,14 @@ public class FormController {
 	}
 	
 	@RequestMapping(value="/test2")
-	public String text2(Model model, @RequestParam("id") String id, @RequestParam("name") String name){
+	public String test2(Model model, @RequestParam("id") String id, @RequestParam("name") String name){
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		return "test2";
 	}
 	
 	@RequestMapping(value="/test3")
-	public String text3(Member member, Model model){
+	public String test3(Member member, Model model){
 		// 파라미터와 일치하는 빈을 만들어서 매개변수로 사용할 수 있습니다.
 		// 전달된 파라미터는 매개변수 빈에 자동으로 입력됩니다.
 		// View 페이지에서 model을 사용하지 않고 member 빈을 전달합니다.
@@ -40,5 +41,12 @@ public class FormController {
 		// member.setName("tiger");
 		// model.addAttribute("member", member);
 		return "test3";
+	}
+	
+	@RequestMapping(value="/test4/{studentid}/{name}")
+	public String test4(Model model, @PathVariable String studentid, @PathVariable String name){
+		model.addAttribute("id", studentid);
+		model.addAttribute("name", name);
+		return "test4";
 	}
 }

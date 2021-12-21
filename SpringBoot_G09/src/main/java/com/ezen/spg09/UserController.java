095939062e590bd76c049d2dcc55d47cc7cdedf2
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-21 18:35:36 +0900

    UserController "/" Mapping Write & UserDto, Dao Create

--- SpringBoot_G09/src/main/java/com/ezen/spg09/UserController.java
+++ SpringBoot_G09/src/main/java/com/ezen/spg09/UserController.java
@@ -1,5 +1,25 @@
 package com.ezen.spg09;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserDao udao;
+	
+	// main
+	@RequestMapping(value="/")
+	public String userListPage(Model model) {
+		// user list 조회
+		List<UserDto> list = udao.list();
+		
+		// userlist를 model 저장 후 userlist.jsp 이동
+		model.addAttribute("users", list);
+		return "userlist";
+	}
 }

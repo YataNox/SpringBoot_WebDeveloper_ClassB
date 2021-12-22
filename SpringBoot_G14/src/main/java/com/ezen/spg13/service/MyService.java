56aab1c3809b33072e8240dc2775c4997344f527
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-22 21:01:43 +0900

    @Transactional Annotation Add

--- SpringBoot_G14/src/main/java/com/ezen/spg13/service/MyService.java
+++ SpringBoot_G14/src/main/java/com/ezen/spg13/service/MyService.java
@@ -2,9 +2,11 @@ package com.ezen.spg13.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.ezen.spg13.dao.ITransactionDao1;
 import com.ezen.spg13.dao.ITransactionDao2;
@@ -18,28 +20,27 @@ public class MyService {
 	ITransactionDao2 td2;
 	
 	@Autowired
-	PlatformTransactionManager ptm;
+	TransactionTemplate tt;
 	
-	@Autowired
-	TransactionDefinition td;
-
+	@Transactional(propagation=Propagation.REQUIRED) // 관련작업 모두 취소
+	// @Transactional(propagation=Propagation.REQUIRED_NEW) // 관련작업 독립실행
 	public int buy(String id, int amount, String error) {
-		int n = 0;
-		
-		// 트랜잭션의 시작
-		TransactionStatus status = ptm.getTransaction(td);
 		try {
-			td1.buy(id, amount);
-			if(error.equals("1"))
-				n = 10 / 0; // error 값이 1이면 강제 에러 발생
-			td2.buy(id, amount);
-			System.out.println("에러 없이 둘 다 실행되었습니다.");
-			ptm.commit(status);
+			tt.execute(new TransactionCallbackWithoutResult() {
+				
+				@Override
+				protected void doInTransactionWithoutResult(TransactionStatus status) {
+					td1.buy(id, amount);
+					int n = 0;
+					if(error.equals("1"))
+						n = 10 / 0; // error 값이 1이면 강제 에러 발생
+					td2.buy(id, amount);
+					System.out.println("Transaction Commit");
+				}
+			});
 			return 1;
 		}catch(Exception e) {
-			// System.out.println("중간에 에러나서 하나만 실행되었습니다.");
-			System.out.println("중간에 에러나서 둘 다 실행이 안되었습니다.");
-			ptm.rollback(status);
+			System.out.println("Transaction RollBack");
 			return 0;
 		}
 	}

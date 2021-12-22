c70b6920563bffd0100bb123db07c6798dc0acab
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-22 21:52:14 +0900

    Controller, Service content Write

--- SpringBoot_G15/src/main/java/com/ezen/spg15/service/MyService.java
+++ SpringBoot_G15/src/main/java/com/ezen/spg15/service/MyService.java
@@ -2,6 +2,11 @@ package com.ezen.spg15.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.ezen.spg15.dao.ITransactionDao1;
 import com.ezen.spg15.dao.ITransactionDao2;
@@ -17,4 +22,28 @@ public class MyService {
 	
 	@Autowired
 	ITransactionDao3 td3;
+	
+	@Autowired
+	TransactionTemplate tt;
+
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public int buy(String id, int amount, String error) {
+		try {
+			tt.execute(new TransactionCallbackWithoutResult() {
+				@Override
+				protected void doInTransactionWithoutResult(TransactionStatus status) {
+					td1.buy(id, amount);
+					if(error.equals("1")) {
+						int n = 10 / 0;
+					}
+					td2.buy(id, amount);
+					System.out.println("Transaction #1 Commit");
+				}
+			});
+			return 1;
+		}catch(Exception e) {
+			System.out.println("Transaction #1 RollBack");
+			return 0;
+		}
+	}
 }

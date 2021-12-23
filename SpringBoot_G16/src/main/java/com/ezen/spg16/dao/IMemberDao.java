fe7a2087142541701ba888371ced8d3b4f5b3b61
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-23 17:19:51 +0900

    getMember Method Write

--- SpringBoot_G16/src/main/java/com/ezen/spg16/dao/IMemberDao.java
+++ SpringBoot_G16/src/main/java/com/ezen/spg16/dao/IMemberDao.java
@@ -2,7 +2,9 @@ package com.ezen.spg16.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ezen.spg16.dto.MemberVO;
+
 @Mapper
 public interface IMemberDao {
-	
+	public MemberVO getMember(String id);
 }

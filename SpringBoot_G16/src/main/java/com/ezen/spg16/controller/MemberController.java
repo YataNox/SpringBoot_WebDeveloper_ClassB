b7062486426016ec65ac063cb63f3bbb09cbdaae
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-23 16:55:06 +0900

    /login Mapping Method Write

--- SpringBoot_G16/src/main/java/com/ezen/spg16/controller/MemberController.java
+++ SpringBoot_G16/src/main/java/com/ezen/spg16/controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.ezen.spg16.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ezen.spg16.dto.MemberVO;
 import com.ezen.spg16.service.MemberService;
 
 @Controller
@@ -11,8 +19,30 @@ public class MemberController {
 	@Autowired
 	MemberService ms;
 	
+	// 로그인창이동
 	@RequestMapping(value = "/")
 	public String index() {
 		return "loginForm";
 	}
+	
+	// 로그인
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(@ModelAttribute("dto") @Valid MemberVO membervo, 
+			BindingResult result, Model model, HttpServletRequest request) {
+		
+		// 로그인시 에러가 있을 때
+		if(result.hasErrors()) { 
+			// 해당 에러가 id와 pw 관련이라면 로그인 창으로 돌아간다.
+			if(result.getFieldError("id") != null) {
+				model.addAttribute("message", result.getFieldError("id").getDefaultMessage());
+				return "loginForm";
+			}else if(result.getFieldError("pw") != null) {
+				model.addAttribute("message", result.getFieldError("pw").getDefaultMessage());
+				return "loginForm";
+			}
+		}
+		
+		return "main";
+	}
+	
 }

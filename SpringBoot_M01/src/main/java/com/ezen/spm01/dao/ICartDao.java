57d56d4755288dd05245dc66cdf4551e87458da7
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-29 18:25:10 +0900

    /cartInsert, /cartList Part Write

--- SpringBoot_M01/src/main/java/com/ezen/spm01/dao/ICartDao.java
+++ SpringBoot_M01/src/main/java/com/ezen/spm01/dao/ICartDao.java
@@ -1,8 +1,14 @@
 package com.ezen.spm01.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ezen.spm01.dto.CartVO;
 
 @Mapper
 public interface ICartDao {
-	
+	public void insertCart(CartVO cvo);
+
+	public ArrayList<CartVO> listCart(String id);
 }
